5c58da3f02c048188ccddac1e990f12a559b557b
NUVA <102314039+NuvaGit@users.noreply.github.com>
2023-01-31 15:44:23 +0000

    modified:   bin/GamePanel.class 	modified:   bin/Player/player.class 	modified:   src/GamePanel.java 	modified:   src/Player/player.java

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,4 +1,23 @@
+import java.util.Scanner;
+
+import Player.player;
 
 public class GamePanel {
+public static void main(String[] args) {
+	player player = new player();
+	Scanner in = new Scanner(System.in);
+	System.out.println("enter number of players");
+	int x = in.nextInt();
+	if(x <2 || x>4) {
+		throw new IllegalArgumentException("invalid nuber of players");
+	}
+	player.setPlayerCount(x);
+	 x = 0;
 
+	 
+	 
+	 
+	 
+	 in.close();	
+}
 }
--- src/Player/player.java
+++ src/Player/player.java
@@ -1,5 +1,26 @@
 package Player;
 
+import java.util.ArrayList;
+
 public class player {
+int playerCount;
+ArrayList<String> names = new ArrayList<String>();
+
+
+public int getPlayerCount() {
+	return this.playerCount;
+}
+
+public void addPLayer(String g) {
+	names.add(g);
+}
+public String getPLayername(int x) {
+	return this.names.get(x);
+}
+public void setPlayerCount(int x) {
+	// TODO Auto-generated method stub
+	this.playerCount = x;
+
+}
 
 }
