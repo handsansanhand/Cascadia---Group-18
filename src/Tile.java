e68701203edd6e13e189626507aaa0781890be09
fmega698 <102313516+fmega698@users.noreply.github.com>
2023-02-06 17:04:37 +0000

    deleted duplicate

--- src/Tile.java
+++ src/Tile.java
@@ -5,22 +5,7 @@ public class Tile { // class for the tiles that will be inserted onto the board
     Tile(ht landType){
         this.landType = landType;
     }
-    public Tile starterTiles(){ //returns a tile object of random habitat tile
-        switch((int) Math.round(Math.random()*4)){
-            case 0:
-                return new Tile(ht.Mountain);
-            case 1:
-                return new Tile(ht.Forest);
-            case 2:
-                return new Tile(ht.Prairie);
-            case 3:
-                return new Tile(ht.Wetland);
-            case 4:
-                return new Tile(ht.River);
-            default:
-                return new Tile(ht.Error);
-        }
-    }
+   
     public Tile starterTiles(){ //returns a tile object of random habitat tile
         switch((int) Math.round(Math.random()*4)){
             case 0:
