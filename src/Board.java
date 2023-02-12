e403aecf88cea6a252fe832bb62c84b6a9348628
fmega698 <102313516+fmega698@users.noreply.github.com>
2023-02-12 17:18:58 +0000

    Update Board.java
    board now contains randomised starter tiles

--- src/Board.java
+++ src/Board.java
@@ -7,35 +7,7 @@ public class Board { // class for the board object for each player
 
     public Board()
     {
-        switch((int) Math.round(Math.random()*5)){ //basic starter tiles, will be changed later to have the half-half ones
-            case 0:
-                TileList.add(new Tile(ht.Forest));
-                TileList.add(new Tile(ht.Wetland));
-                TileList.add(new Tile(ht.Mountain));
-                break;
-            case 1:
-                TileList.add(new Tile(ht.Wetland));
-                TileList.add(new Tile(ht.Mountain));
-                TileList.add(new Tile(ht.Prairie));
-                break;
-            case 2:
-                TileList.add(new Tile(ht.River));
-                TileList.add(new Tile(ht.Forest));
-                TileList.add(new Tile(ht.Prairie));
-                break;
-            case 3:
-                TileList.add(new Tile(ht.Forest));
-                TileList.add(new Tile(ht.River));
-                TileList.add(new Tile(ht.Wetland));
-                break;
-            case 4:
-                TileList.add(new Tile(ht.Wetland));
-                TileList.add(new Tile(ht.Prairie));
-                TileList.add(new Tile(ht.Mountain));
-                break;
-            default:
-                break;
-        }
+       TileList.add(Tile.starterTiles());
     }
 
     public String toString(){ //visual representation of board here
