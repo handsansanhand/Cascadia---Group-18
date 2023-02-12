251c0b5924897f72e51d87b8b6ab94f6cde1c3d1
fmega698 <102313516+fmega698@users.noreply.github.com>
2023-02-12 17:16:46 +0000

    need change starter tile function
    in board.java

--- src/Board.java
+++ src/Board.java
@@ -1,8 +1,8 @@
 import java.util.ArrayList;
 public class Board { // class for the board object for each player
     ArrayList<Tile> TileList = new ArrayList<Tile>();
-    public void addTile(ht x){
-        TileList.add(new Tile(x));
+    public void addTile(ht x, ht y){
+        TileList.add(new Tile(x, y));
     }
 
     public Board()
