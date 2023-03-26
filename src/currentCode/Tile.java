e15967a03a81b3883eb311768b879159d83ba718
handsansanhand <102315833+handsansanhand@users.noreply.github.com>
2023-03-26 16:53:11 +0100

    more names

--- src/currentCode/Tile.java
+++ src/currentCode/Tile.java
@@ -1,3 +1,9 @@
+/*
+Student Number - Name - Github ID
+21467574 - Jack Wright - #102315833
+21447182 - Jack Neilan - #102314039
+21350866 - Trystan Rothery - #102313516
+*/
 package currentCode;
 
 public class Tile { // class for the tiles that will be inserted onto the board
